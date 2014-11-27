7c4d28f3de60855b613a0fbb74e923865a6efdab
Praxthisnovcht <praxthisnovcht@gmail.com>
2014-11-27 15:02:00 +0000

    Update CustomChatListener.php

--- src/Praxthisnovcht/CustomChat/CustomChatListener.php
+++ src/Praxthisnovcht/CustomChat/CustomChatListener.php
@@ -38,5 +38,64 @@ class CustomChatListener implements Listener {
 				$event->setCancelled ( true );
 				return;
 			}
-}
+			$format = $this->getFormattedMessage ( $player, $event->getMessage () );
+			$config_node = $this->pgin->getConfig ()->get ( "enable-formatter" );
+			if (isset ( $config_node ) and $config_node === true) {
+				$event->setFormat ( $format );
+			}
+			return;
+		}
+	}
+	public function onPlayerJoin(PlayerJoinEvent $event) {
+		$player = $event->getPlayer ();
+		$this->pgin->formatterPlayerDisplayName ( $player );
+	}
+// 	public function formatterPlayerDisplayName(Player $p) {
+// 		$playerPrefix = $this->pgin->getConfig ()->get ( $player->getName () );
+// 		$defaultPrefix = $this->pgin->getConfig ()->get ( "default-player-prefix" );
+		
+// 		if ($playerPrefix != null) {
+// 			$p->setDisplayName ( $playerPrefix . ":" . $name );
+// 			return;
+// 		}
+		
+// 		if ($defaultPrefix != null) {
+// 			$p->setDisplayName ( $defaultPrefix . ":" . $name );
+// 			return;
+// 		}
+// 	}
+	
+	public function getFormattedMessage(Player $player, $message) {
+		$format = $this->pgin->getConfig ()->get ( "chat-format" );
+		// $format = "<{PREFIX} {USER_NAME}> {MESSAGE}";		
+		$format = str_replace ( "{WORLD_NAME}", $player->getLevel ()->getName (), $format );
+		
+		$nick = $this->pgin->getConfig ()->get ( $player->getName () > ".nick");
+		if ($nick!=null) {
+			$format = str_replace ( "{DISPLAY_NAME}", $nick, $format );
+		} else {
+			$format = str_replace ( "{DISPLAY_NAME}", $player->getName (), $format );			
+		}
+		
+		$format = str_replace ( "{MESSAGE}", $message, $format );
+		
+		$level = $player->getLevel ()->getName ();
+		
+		$prefix = null;
+		$playerPrefix = $this->pgin->getConfig ()->get ( $player->getName ().".prefix" );
+		if ($playerPrefix != null) {
+			$prefix = $playerPrefix;
+		} else {
+			//use default prefix
+			$prefix = $this->pgin->getConfig ()->get ( "default-player-prefix");
+		}				
+		if ($prefix == null) {
+			$prefix = "";
+		}
+		$format = str_replace ( "{PREFIX}", $prefix, $format );
+		return $format;
+	}
+	private function log($msg) {
+		$this->pgin->getLogger ()->info ( $msg );
+	}
 }
